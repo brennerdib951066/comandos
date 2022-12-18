f99c9baff2a9b35a969831682895e8a0d4f688d0
brennerdib951066 <brennerbne@gmail.com>
2022-12-18 12:08:11 -0300

    t

---
+++ gitHub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Verifica se o arquivo especificado existe
+if [ ! -f "$1" ]; then
+echo "Arquivo não encontrado."
+exit 1
+fi
+
+#Adiciona o arquivo ao stage do git
+git add "$1"
+
+#Faz o commit com a mensagem especificada
+git commit -m "Enviando arquivo $1 para o GitHub"
+
+#Envia o commit para o repositório remoto
+git push origin master
